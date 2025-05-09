be2e8cf59e9b225bd0d186dbdb3c48fb0d847493
Rudranil Das <rudranild04@gmail.com>
2025-05-09 20:41:43 +0530

    Updated firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,14 +5,15 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAs3iszGjqThZYKWILC8DLXBR3c2F5j-zs",
-  authDomain: "greenlens-ai-bd442.firebaseapp.com",
-  projectId: "greenlens-ai-bd442",
-  storageBucket: "greenlens-ai-bd442.appspot.com",
-  messagingSenderId: "1045452202792",
-  appId: "1:1045452202792:web:e11309e8c94218bbbda61d",
-  measurementId: "G-DNLX2F101G"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
