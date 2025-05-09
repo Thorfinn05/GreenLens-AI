d0304b217df3c7036ea5d399ebc8902efb36d411
Rudranil Das <rudranild04@gmail.com>
2025-05-09 20:27:15 +0530

    Updated firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAs3iszGjqThZYKWILC8DLXBR3c2F5j-zs",
   authDomain: "greenlens-ai-bd442.firebaseapp.com",
   projectId: "greenlens-ai-bd442",
-  storageBucket: "greenlens-ai-bd442.firebasestorage.app",
+  storageBucket: "greenlens-ai-bd442.appspot.com",
   messagingSenderId: "1045452202792",
   appId: "1:1045452202792:web:e11309e8c94218bbbda61d",
   measurementId: "G-DNLX2F101G"
@@ -17,5 +18,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 
 export default app;
